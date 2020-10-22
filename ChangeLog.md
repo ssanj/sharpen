9471061b4ae5fbfee4401b38f5cac3b87b485739
sanjiv sahayam <sanjsmailbox@gmail.com>
2020-10-22 22:27:01 +1100

    Initial commit

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for voelm
+
+## Unreleased changes
---
+++ src/Lib.hs
@@ -0,0 +1,87 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Lib
+    ( someFunc
+    ) where
+
+import qualified Data.Text    as T
+import qualified Data.Text.IO as T
+import qualified Data.ByteString.Lazy as B
+import Control.Applicative ((<|>))
+
+import GHC.Generics
+import Data.Aeson
+import Data.Aeson.Types (Parser)
+import Data.Aeson.Casing (aesonPrefix, camelCase)
+
+sample1 :: B.ByteString
+sample1 = "{\"type\":\"compile-errors\",\"errors\":[{\"path\":\"/Volumes/Work/projects/code/frontend/scrib-elm/src/StorageKeys.elm\",\"name\":\"StorageKeys\",\"problems\":[{\"title\":\"RESERVED WORD\",\"region\":{\"start\":{\"line\":78,\"column\":5},\"end\":{\"line\":78,\"column\":9}},\"message\":[\"It looks like you are trying to use `case` in this pattern:\n\n78|     case Save   ->  E.string \\\"save\\\"\n        \",{\"bold\":false,\"underline\":false,\"color\":\"RED\",\"string\":\"^^^^\"},\"\nThis is a reserved word! Try using some other name?\"]}]}]}"
+
+data LineAndColumn = LineAndColumn { lineandcolumnLine :: Int, lineandcolumnColumn :: Int } deriving stock (Generic, Show)
+
+data Region = Region { regionStart :: LineAndColumn, regionEnd :: LineAndColumn  } deriving stock (Generic, Show)
+
+data Problem = Problem { problemTitle :: T.Text, problemRegion :: Region, problemMessage :: [Message] } deriving stock (Generic, Show)
+
+data Error = Error  { errorPath :: T.Text, errorName :: T.Text, errorProblems :: [Problem] } deriving stock (Generic, Show)
+
+data CompilerOutput = CompilerOutput { compileroutputErrors :: [Error],  compileroutputType :: T.Text }  deriving stock (Generic, Show)
+
+data Message = MessageLine MessageText
+             | MessageFormatting MessageFormat  deriving stock Show
+
+newtype MessageText = MessageText T.Text deriving stock Show
+
+data MessageFormat =
+  MessageFormat {
+    messageformatBold :: Bool
+  , messageformatUnderline :: Bool
+  , messageformatColor :: T.Text
+  , messageformatString :: T.Text
+  }  deriving stock (Generic, Show)
+
+jsonOptions :: Options
+jsonOptions = aesonPrefix camelCase
+
+instance FromJSON Message where
+  parseJSON = \v ->
+    let messageLineParser       :: Value -> Parser Message = \v -> fmap MessageLine (parseJSON v :: Parser MessageText)
+        messageFormattingParser :: Value -> Parser Message = \v -> fmap MessageFormatting (parseJSON v :: Parser MessageFormat)
+    in (messageLineParser v) <|> (messageFormattingParser v)
+
+instance FromJSON MessageText where
+  parseJSON = withText "MessageText" (pure . MessageText)
+
+instance FromJSON MessageFormat where
+  parseJSON = withObject "MessageFormat" $ \v ->
+    MessageFormat
+      <$> v .: "bold"
+      <*> v .: "underline"
+      <*> v .: "color"
+      <*> v .: "string"
+
+instance FromJSON LineAndColumn where
+   parseJSON = genericParseJSON jsonOptions
+
+instance FromJSON Region where
+   parseJSON = genericParseJSON jsonOptions
+
+instance FromJSON Problem where
+   parseJSON = genericParseJSON jsonOptions
+
+instance FromJSON Error where
+   parseJSON = genericParseJSON jsonOptions
+
+instance FromJSON CompilerOutput where
+   parseJSON = genericParseJSON jsonOptions
+
+
+someFunc :: IO ()
+someFunc =
+  let result = eitherDecode sample1 :: Either String CompilerOutput
+  in T.putStrLn . T.pack . show $ result
+
+
