6565092dff6238bb3f7f28a02eb974fd46643a7a
sanjiv sahayam <sanjsmailbox@gmail.com>
2021-06-01 08:42:32 +1000

    Expand test on Parser

--- test/CompilerErrorParserSpec.hs
+++ test/CompilerErrorParserSpec.hs
@@ -12,11 +12,24 @@ import Test.Tasty.HUnit     (assertFailure, (@?=), Assertion)
 import qualified Data.Text     as T
 import qualified Data.Text.IO  as T
 
+
 unit_decodeCompilerError :: Assertion
 unit_decodeCompilerError = do
     input <- getInput "resources/compiler-error-1.json"
     let decodedE = decodeInput input :: Either String CompilerOutput
-    either (\e -> assertFailure ("Could not decode input: " <> show e)) (\_ -> True @?= True) decodedE
+    either failWithError assertCompilerOutput decodedE
+
+
+failWithError :: String -> Assertion
+failWithError e =
+    let errorMessage = show e
+    in assertFailure ("Could not decode input: " <> errorMessage)
+
+
+assertCompilerOutput :: CompilerOutput -> Assertion
+assertCompilerOutput co = do
+    "compile-errors" @?= (compileroutputType co)
+
 
 getInput :: String -> IO T.Text
 getInput = T.readFile
