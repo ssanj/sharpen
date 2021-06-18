32d23be4ef611241fc926ca7d0ea0478baba76b1
sanjiv sahayam <sanjsmailbox@gmail.com>
2021-06-18 21:35:27 +1000

    Inline functions into let

--- src/ElmCompilerError.hs
+++ src/ElmCompilerError.hs
@@ -15,9 +15,40 @@ import qualified Data.List.NonEmpty as N
 
 processError :: RuntimeConfig -> CompilerError -> IO ()
 processError RuntimeConfig { runtimeConfigColorMap = colorMap, runtimeConfigConfig = config } compilerError =
-  let stats                = configStats config
+  let compilerErrorToProblemsAtFileLocations :: CompilerError -> N.NonEmpty ProblemsAtFileLocation
+      compilerErrorToProblemsAtFileLocations (CompilerError nonEmptyErrors _) = processCompilerErrors =<< nonEmptyErrors
+
+
+      processCompilerErrors :: Error -> N.NonEmpty ProblemsAtFileLocation
+      processCompilerErrors (Error filePath _ problems) = problemsAtFileLocation (FilePath filePath) <$> problems
+
+
+      problemsAtFileLocation :: FilePath -> Problem -> ProblemsAtFileLocation
+      problemsAtFileLocation filePath (Problem title (Region (LineAndColumn start end) _) messages) =
+        let problemDescriptions :: N.NonEmpty ProblemDescription = fmap problemDescription messages
+        in ProblemsAtFileLocation (Title title) filePath (start, end) problemDescriptions
+
+
+      problemDescription ::  Message -> ProblemDescription
+      problemDescription (MessageLine (MessageText messageText)) = ProblemDescription [] messageText
+      problemDescription (MessageFormatting MessageFormat {messageformatBold = doBold, messageformatUnderline = doUnderline, messageformatColor = doColor, messageformatString = messageText}) =
+        let formatting =
+              catMaybes
+                [
+                  boolToMaybe doBold BoldFormat
+                , boolToMaybe doUnderline UnderlineFormat
+                , ColourFormat  <$> (doColor >>= maybeColor colorMap)
+                ]
+        in ProblemDescription formatting messageText
+
+
+      filterByRequested :: NumberOfErrors -> N.NonEmpty ProblemsAtFileLocation ->  N.NonEmpty ProblemsAtFileLocation
+      filterByRequested AllErrors = id
+      filterByRequested OneError  = pure . N.head
+
+      stats                = configStats config
       numErrors            = configNumErrors config
-      problems             = compilerErrorToProblemsAtFileLocations colorMap compilerError
+      problems             = compilerErrorToProblemsAtFileLocations compilerError
       problemsToDisplay    = filterByRequested numErrors problems
       numProblemsDisplayed = N.length problemsToDisplay
       compilerErrorDesc    = CompilerErrorDescription problemsToDisplay
@@ -25,36 +56,3 @@ processError RuntimeConfig { runtimeConfigColorMap = colorMap, runtimeConfigConf
       renderEnabledStats   = renderStats stats numProblemsDisplayed
       renderCompilerErrors = renderCompilerErrorDescription compilerErrorDesc
   in renderCompilerErrors >> renderEnabledStats
-
-
-compilerErrorToProblemsAtFileLocations :: ColorMap -> CompilerError -> N.NonEmpty ProblemsAtFileLocation
-compilerErrorToProblemsAtFileLocations cm (CompilerError nonEmptyErrors _) = (processCompilerErrors cm) =<< nonEmptyErrors
-
-
-processCompilerErrors :: ColorMap -> Error -> N.NonEmpty ProblemsAtFileLocation
-processCompilerErrors cm (Error filePath _ problems) = problemsAtFileLocation cm (FilePath filePath) <$> problems
-
-
-problemsAtFileLocation :: ColorMap -> FilePath -> Problem -> ProblemsAtFileLocation
-problemsAtFileLocation cm filePath (Problem title (Region (LineAndColumn start end) _) messages) =
-  let problemDescriptions :: N.NonEmpty ProblemDescription = fmap (problemDescription cm) messages
-  in ProblemsAtFileLocation (Title title) filePath (start, end) problemDescriptions
-
-
-problemDescription ::  ColorMap -> Message -> ProblemDescription
-problemDescription _ (MessageLine (MessageText messageText)) = ProblemDescription [] messageText
-problemDescription colorNamesMap (MessageFormatting MessageFormat {messageformatBold = doBold, messageformatUnderline = doUnderline, messageformatColor = doColor, messageformatString = messageText}) =
-  let formatting =
-        catMaybes
-          [
-            boolToMaybe doBold BoldFormat
-          , boolToMaybe doUnderline UnderlineFormat
-          , ColourFormat  <$> (doColor >>= maybeColor colorNamesMap)
-          ]
-  in ProblemDescription formatting messageText
-
-
-filterByRequested :: NumberOfErrors -> N.NonEmpty ProblemsAtFileLocation ->  N.NonEmpty ProblemsAtFileLocation
-filterByRequested AllErrors = id
-filterByRequested OneError  = pure . N.head
-
