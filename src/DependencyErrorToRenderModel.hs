4f7a3fee173aa23d36d2ef1a88eecb7de5e64128
sanjiv sahayam <sanjsmailbox@gmail.com>
2021-07-05 20:55:05 +1000

    WIP

---
+++ src/DependencyErrorToRenderModel.hs
@@ -0,0 +1,26 @@
+module DependencyErrorToRenderModel (processError2) where
+
+import Model
+import RenderModel
+import Theme
+
+import Data.Maybe (catMaybes)
+
+processError2 :: DependencyError -> IO ()
+processError2 (DependencyError path title nonEmptyMessages) =
+  let problems = problemDescription <$> nonEmptyMessages
+      genProblemsInFile = GeneralProblemsInFile (Title title) (FilePath path) problems
+  in renderGeneralProblemsInFile genProblemsInFile
+
+
+problemDescription :: Message -> ProblemDescription
+problemDescription (MessageLine (MessageText messageText)) = ProblemDescription [] messageText
+problemDescription (MessageFormatting MessageFormat {messageformatBold = doBold, messageformatUnderline = doUnderline, messageformatColor = doColor, messageformatString = messageText}) =
+  let formatting =
+        catMaybes
+          [
+            boolToMaybe doBold BoldFormat
+          , boolToMaybe doUnderline UnderlineFormat
+          , ColorFormat  <$> (doColor >>= colorToMaybeColorType)
+          ]
+  in ProblemDescription formatting messageText
---
+++ src/Render/CompilerErrorDescriptionRender.hs
@@ -0,0 +1,6 @@
+module Render.ElmCompilerErrorRender where
+
+import RenderModel (CompilerErrorDescription)
+
+render :: CompilerErrorDescription -> IO ()
+render = undefined
