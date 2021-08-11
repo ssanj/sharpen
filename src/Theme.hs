24e2581de63beaf3f3231cac1b3666a0e8db2583
sanjiv sahayam <sanjsmailbox@gmail.com>
2021-08-12 09:42:56 +1000

    Refactor Theme

--- src/Theme.hs
+++ src/Theme.hs
@@ -80,6 +80,12 @@ renderProblem (ProblemDescription formatting message) =
   in formattingApplied >> T.putStr message >> resetAnsi
 
 
+renderProblem2 :: Printer -> ProblemDescription -> IO ()
+renderProblem2 printer (ProblemDescription formatting message) =
+  let formattingApplied = traverse_ (printerRenderFormatting printer) formatting
+  in formattingApplied >> T.putStr message >> (printerResetAnsi printer)
+
+
 renderFileProblems :: ProblemsAtFileLocation -> IO ()
 renderFileProblems pfl@(ProblemsAtFileLocation _ _ _ problems) =
   do
@@ -87,12 +93,25 @@ renderFileProblems pfl@(ProblemsAtFileLocation _ _ _ problems) =
     newLines 1
     traverse_  renderProblem problems
 
+renderFileProblems2 :: Printer -> ProblemsAtFileLocation -> IO ()
+renderFileProblems2 printer pfl@(ProblemsAtFileLocation _ _ _ problems) =
+  do
+    createTitleAndFile2 printer pfl
+    printerParagraph printer
+    traverse_  (renderProblem2 printer) problems
+
 
 createTitleAndFile :: ProblemsAtFileLocation -> IO ()
 createTitleAndFile (ProblemsAtFileLocation title filePath (s, e) _) =
   let coords = showt s <> ":" <> showt e
       singleFileMessage = "-- " <> showt title <> " ---------- " <> showt filePath <> ":" <> coords
   in withColourInline singleFileMessage titleColor >> newLines 1
+
+createTitleAndFile2 :: Printer -> ProblemsAtFileLocation -> IO ()
+createTitleAndFile2 printer (ProblemsAtFileLocation title filePath (s, e) _) =
+  let coords = showt s <> ":" <> showt e
+      singleFileMessage = "-- " <> showt title <> " ---------- " <> showt filePath <> ":" <> coords
+  in (printerTitleColor printer $ singleFileMessage) >> (printerParagraph printer)
 
 
 renderGeneralErrorHeader :: DependencyErrorDescription -> IO ()
@@ -118,6 +137,12 @@ renderCompilerErrorDescription (CompilerErrorDescription errorDescriptions) = do
   traverse_ (\ed -> newLines 2 >> renderFileProblems ed) errorDescriptions
   newLines 2
 
+renderCompilerErrorDescription2 :: Printer -> CompilerErrorDescription ->  IO ()
+renderCompilerErrorDescription2 printer (CompilerErrorDescription errorDescriptions) = do
+  let borderX = printerBorder printer
+  traverse_ (\ed -> borderX >> renderFileProblems2 printer ed) errorDescriptions
+  borderX
+
 
 renderGeneralProblemsInFile :: DependencyErrorDescription  -> IO ()
 renderGeneralProblemsInFile probsInFile =
@@ -129,24 +154,68 @@ renderGeneralProblemsInFile probsInFile =
     border
 
 
-border2 :: ColorTheme -> IO ()
-border2 ColorTheme {colorThemeBorderLines=blines} = newLines blines
-
-paragraph2 :: ColorTheme ->  IO ()
-paragraph2 ColorTheme {colorThemeParagraphLines=plines} = newLines plines
-
-renderGeneralErrorHeader2 :: ColorTheme -> DependencyErrorDescription -> IO ()
-renderGeneralErrorHeader2 colorTheme (DependencyErrorDescription title path _) =
+renderGeneralErrorHeader2 :: Printer -> DependencyErrorDescription -> IO ()
+renderGeneralErrorHeader2 printer (DependencyErrorDescription title path _) =
   let heading     = "-- " <> showt title <> " ---------- " <> showt path
-      titleColor2 = colorThemeTitleColor colorTheme
-  in withColourInline heading titleColor2 >> paragraph2 colorTheme
+      titleColor2 = printerTitleColor printer $ heading
+      paragraph3  = printerParagraph printer
+  in titleColor2 >> paragraph3
 
 
-renderGeneralProblemsInFile2 :: ColorTheme -> DependencyErrorDescription  -> IO ()
-renderGeneralProblemsInFile2 colorTheme probsInFile =
+renderGeneralProblemsInFile2 :: Printer -> DependencyErrorDescription  -> IO ()
+renderGeneralProblemsInFile2 printer probsInFile =
   do
-    border2 colorTheme
-    renderGeneralErrorHeader2 colorTheme probsInFile
-    paragraph2 colorTheme
-    traverse_  renderProblem $ dependencyErrorDescriptionPathProblemDescriptions probsInFile
-    border2 colorTheme
+    printerBorder printer
+    renderGeneralErrorHeader2 printer probsInFile
+    printerParagraph printer
+    traverse_  (renderProblem2 printer) $ dependencyErrorDescriptionPathProblemDescriptions probsInFile
+    printerBorder printer
+
+
+fromTheme :: ColorTheme -> Printer
+fromTheme theme =
+  let withColourInlineX :: T.Text -> Color -> IO ()
+      withColourInlineX text color = do
+        setSGR [SetColor Foreground Dull color]
+        T.putStr text
+        resetAnsi
+
+      newLinesX :: Int -> IO ()
+      newLinesX n = traverse_ (const $ T.putStrLn "") [1..n]
+
+      colorTypeToColorX :: ColorType -> Color
+      colorTypeToColorX  CompilerErrorColor      = colorThemeErrorColor theme
+      colorTypeToColorX  CompilerSuggestionColor = colorThemeSuggestionColor theme
+      colorTypeToColorX  DependencyErrorColor    = colorThemeDependencyErrorColor theme
+
+      renderFormattingX :: MessageFormatType -> IO ()
+      renderFormattingX (ColorFormat colorType) =
+        let colorsToSGR :: Color -> [SGR]
+            colorsToSGR color = [SetColor Foreground Dull color]
+
+            sgrColors :: [SGR]
+            sgrColors = colorsToSGR $ colorTypeToColorX colorType
+
+        in setSGR sgrColors
+      renderFormattingX UnderlineFormat          = setSGR [SetUnderlining SingleUnderline]
+      renderFormattingX BoldFormat               = setSGR [SetConsoleIntensity BoldIntensity]
+
+  in
+    Printer {
+      printerBorder           = newLinesX $ colorThemeBorderLines theme
+    , printerParagraph        = newLinesX $ colorThemeParagraphLines theme
+    , printerTitleColor       = \title ->  withColourInlineX title (colorThemeTitleColor theme) >> newLinesX 1
+    , printerResetAnsi        = setSGR [Reset]
+    , printerRenderFormatting = renderFormattingX
+    , printerNewLines         = newLinesX
+    }
+
+data Printer =
+  Printer {
+    printerBorder           :: IO ()
+  , printerParagraph        :: IO ()
+  , printerTitleColor       :: T.Text -> IO ()
+  , printerResetAnsi        :: IO ()
+  , printerRenderFormatting :: MessageFormatType -> IO ()
+  , printerNewLines         :: Int -> IO ()
+  }
